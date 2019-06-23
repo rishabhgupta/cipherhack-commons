7a582ae39747f9c881aa11dd16d69dcf232a197c
rishabhgupta <rishabh@moengage.com>
2019-06-23 15:27:19 +0530

    Updates

---
+++ precache-manifest.79c9a1bff66b59c415bafce748a02c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34a53899564f995adcd68d64d865d34",
+    "url": "/dashboard-commons/index.html"
+  },
+  {
+    "revision": "a5dc9737af82c7e81be2",
+    "url": "/dashboard-commons/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "5fad11140991fff134ea",
+    "url": "/dashboard-commons/static/css/main.4c316954.chunk.css"
+  },
+  {
+    "revision": "a5dc9737af82c7e81be2",
+    "url": "/dashboard-commons/static/js/2.62448630.chunk.js"
+  },
+  {
+    "revision": "5fad11140991fff134ea",
+    "url": "/dashboard-commons/static/js/main.1fe3e86d.chunk.js"
+  },
+  {
+    "revision": "bf6e224aca2cc31b3923",
+    "url": "/dashboard-commons/static/js/runtime~main.aff8923b.js"
+  }
+]);
